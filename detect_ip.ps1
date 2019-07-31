427927260a1cf0644205f9383cb9b3d9ca6e158d
Oleksiy_Pashkovskiy <49391304+alekspv@users.noreply.github.com>
2019-07-31 18:20:46 +0300

    Update detect_ip.ps1
    added  -UseBasicParsing

--- detect_ip.ps1
+++ detect_ip.ps1
@@ -1,2 +1,2 @@
 # this is a dirty prototyping hack and will not work in a real cluster
-(Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
+(Invoke-WebRequest -uri "http://ifconfig.me/ip" -UseBasicParsing).Content
